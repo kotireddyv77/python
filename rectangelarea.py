3e1ec7173da01ebc2aa270ce35e67e15bd310737
koti <kotireddyv99@gmail.com>
2025-03-29 16:15:32 +0530

    updated

---
+++ rectangelarea.py
@@ -0,0 +1,5 @@
+lenght = float(input("Enter the lenght: "))
+width = float(input("Enter the width: "))
+area = lenght * width
+
+print(f"The area of rectangle is: {area}")
---
+++ totalsales.py
@@ -0,0 +1,7 @@
+item = input("Enter the name of item: ")
+price = float(input("Enter the price of each item: "))
+quantity = int(input("Enter the total number of items: "))
+
+sales = price * quantity
+
+print(f"Total sales value is: {sales}")
