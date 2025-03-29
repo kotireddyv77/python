daa27489e65a0c4016921976774a37fc1065270e
koti <kotireddyv99@gmail.com>
2025-03-29 17:24:33 +0530

    arithmetic file added

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+result = 10
+#result = a + 1
+#result += 1
+#result = result - 1
+#result -= 1
+#result = result * 2
+#result *= 2
+#result = result / 2
+#result /= 2
+#result = result % 3
+#result %= 3
+#result = result ** 2
+result **= 2
+
+
+
+print(result)
