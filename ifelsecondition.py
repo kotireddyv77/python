b486d31790a46e8623b7c3c389c03c35a5459bda
koti <kotireddyv99@gmail.com>
2025-03-29 20:10:22 +0530

    ifelsecondition file added

---
+++ ifelsecondition.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age : "))
+
+if age >= 18:
+    print("You are eligible to signup for credit card")
+else:
+    print("You are is not 18+ to signup")
--- math.py
+++
@@ -1,8 +0,0 @@
-import math
-x = 9
-y = 9.1
-
-print(math.pi)
-print(math.sqrt(x))
-print(math.ceil(y))
-print(math.floor(y))
